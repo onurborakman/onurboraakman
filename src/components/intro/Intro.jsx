3440712b2c78e2be9966eb609ca3186f73336aae
Onur Bora Akman <onurboraakman@hotmail.com>
2022-01-18 04:07:08 -0700

    fixed picture

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -20,7 +20,7 @@ export default function Intro() {
         <div className='intro' id='intro'>
             <div className='left'>
                 <div className="imgContainer">
-                    <img src="assets/onurboraakman.jpeg" alt=""/>
+                    <img src="assets/onurboraakman.png" alt=""/>
                 </div>
             </div>
             <div className='right'>
